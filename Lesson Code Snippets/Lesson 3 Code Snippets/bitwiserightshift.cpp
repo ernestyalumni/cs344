c78cb61c3569811338261d41e902e1b4c0afc1ca
ernestyalumni <ernestyalumni@gmail.com>
2016-11-01 17:31:32 -0700

    Added bitwiserightshift update

--- Lesson Code Snippets/Lesson 3 Code Snippets/bitwiserightshift.cpp
+++ Lesson Code Snippets/Lesson 3 Code Snippets/bitwiserightshift.cpp
@@ -54,6 +54,70 @@ int main() {
 		std::cout << digit << " " ; 
 	std::cout << std::endl;
 
+	std::cout << " I will demonstrate what the bitshift operators do, to remind us. " << std::endl;
+	
+	std::vector<int> leftshift_example;
+	for (auto i=0; i<11; ++i) {
+		leftshift_example.push_back(i) ; }
+	std::cout <<   " Originally,             : " ;
+	for (auto elem : leftshift_example ) {
+		std::cout << " " << elem ; } 
+	std::cout << "\n After application of << : " ;
+	for (auto &elem : leftshift_example) {
+		elem <<= 1; 
+		std::cout << " " << elem ; 		}
+	std::cout << "\n So effectively << 1 is the multiplication by 2 " << std::endl;
+	
+	
+	std::vector<int> rightshift_example;
+	for (auto i=0; i<11; ++i) {
+		rightshift_example.push_back(i) ; }
+	std::cout <<   " Originally,             : " ;
+	for (auto elem : rightshift_example ) {
+		std::cout << " " << elem ; } 
+	std::cout << "\n After application of >> : " ;
+	for (auto &elem : rightshift_example) {
+		elem >>= 1; 
+		std::cout << " " << elem ; 		}
+	std::cout << "\n So effectively >> 1 is division by 2 " << std::endl;
 
+	std::cout << " \n Further example of >> 1; we'll loop through a number of times. " << std::endl;
+	for (auto i=11; i<22; ++i) {
+		rightshift_example.push_back(i); 
+		rightshift_example[i-11] = (i-11);
+		}
+	std::cout <<       " Originally,                             : " ;
+	for (auto elem : rightshift_example ) {
+		std::cout << " " << elem ; } 
+	for (auto iter = 0 ; iter < 4 ; ++iter) {
+		std::cout << "\n After application of >> for iteration " << iter << " : " ;
+		for (auto &elem : rightshift_example) {
+			elem >>= 1; 
+			std::cout << " " << elem ; 		}
+	}
+	std::cout << "\n Indeed, consider the application of j = 1,2,3,... for >> j operator on elements - " << std::endl; 
+	for (auto i=0; i<22; ++i) {
+		rightshift_example[i] = i ; }
+	std::cout <<   " Originally,               : " ;
+	for (auto elem : rightshift_example ) {
+		std::cout << " " << elem ; } 
+	for (auto iter = 0 ; iter < 4 ; ++iter) {
+		std::cout << "\n After application of << " << iter +1 << " : " ;
+		for (auto elem : rightshift_example) {
+			std::cout << " " << (elem >> (iter + 1)); 		}
+	}
+
+	std::cout << "\n Also check out modulus arithmetic" << std::endl;
+	std::cout <<   " Originally,                 : " ;
+	for (auto elem : rightshift_example ) {
+		std::cout << " " << elem ; } 
+	for (auto iter = 0 ; iter < 4 ; ++iter) {
+		std::cout << "\n After application of % 2**" << iter+1 << " : " ;
+		for (auto elem : rightshift_example) {
+			std::cout << " " << (elem % (2 << iter) ); 		}
+		std::cout << " with (2 << (iter + 1 )) being : " << (2 << iter  ) << std::endl;
+	}
+	
+	std::cout << std::endl;
 
 }
